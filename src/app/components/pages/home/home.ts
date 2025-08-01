4b6abee947b677a1774f8f21cdee57f689ac4902
Marcus Vinícius <marcus-vnt@live.com>
2025-07-31 22:13:41 -0300

    Home page.

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { HowItWorks } from './how-it-works/how-it-works';
+import { Testimonials } from './testimonials/testimonials';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, HowItWorks],
+  imports: [Hero, HowItWorks, Testimonials],
   templateUrl: './home.html',
   styles: ``,
 })
